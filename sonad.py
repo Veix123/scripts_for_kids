fb6c03c345209d2b5681580be647eb7e8a6b18f2
Veiko Vunder <veix123@gmail.com>
2019-10-16 21:00:45 +0300

    practice irregular verbs

---
+++ sonad.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import csv
+import random   
+import sys
+
+db = []
+
+
+with open('data.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        db.append(row)
+
+random.shuffle(db)
+
+print("Anni inka irregular verbs")
+print("Sisesta kolm vormi ja eralda need tühikuga")
+
+points = 0
+tries= 0
+
+try:
+    for row in db:
+        points_available=1
+        while True:
+            answer = input(row[3]+ ": ")
+            answers = answer.split()
+            
+            tries += 1
+            if(answers == row[0:3]):
+                print("Õige\n")
+                points += points_available
+                break
+            else:
+                print("Vale\n")
+                points_available=0
+except KeyboardInterrupt:
+    print("VALMIS! Punkte kokku: " + str(points) + "/" + str(tries))
+    sys.exit()
+
+
+
+
+print("VALMIS! Punkte kokku: " + str(points) + "/" + str(tries))
